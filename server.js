b53afbd59ae17759d74cf0a48b3e2077265d2f02
Джером <jorbinarojr@gmail.com>
2024-06-29 11:33:57 +0300

    HBS

--- server.js
+++ server.js
@@ -1,16 +1,110 @@
 const http = require('http');
+const path = require('path');
 const express = require('express')
-
+const exphsb = require('express-handlebars');
+const app = express();
  //req - запрос
  //res - ответ сервера
 
- const app = express();
+ const hbs = exphsb.create({
+   defaultLayout: 'main',
+   extname: 'hbs'
+ })
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views');
+
+
  const PORT = process.env.PORT || 3000; 
 
- app.get(PORT, (req, res) => {
-   res.send('RusAnki!');
- });
+ app.get('/', (req, res) => {
+   res.render('home');
+ });  
 
  app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}.`);
  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // :) 11010000 10011000 11010000 10110001 11010000 10110000 11010001 10000010 11010001 10000011 11010000 10111011 11010000 10111000 11010000 10111101 00100000 11010000 10010101 11010000 10110001 11010000 10110000 11010000 10111101 11010000 10110000 11010001 10000010
