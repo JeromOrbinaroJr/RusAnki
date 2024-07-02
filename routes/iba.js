26c3f09737e1c4e0e37cca0f24744517a1b461c4
Джером <jorbinarojr@gmail.com>
2024-07-02 14:58:05 +0300

    .

--- routes/iba.js
+++ routes/iba.js
@@ -2,7 +2,10 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/iba', (req, res) => {
-    res.render('iba');
+    res.render('iba', {
+        title: 'Секретик)',
+        isIba: true
+    });
 })
 
 module.exports = router;
