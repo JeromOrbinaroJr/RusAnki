264a56e1643e5432d7e068ad13946f606163e7a9
Джером <jorbinarojr@gmail.com>
2024-07-03 15:08:48 +0300

    comments

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 });
-
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
